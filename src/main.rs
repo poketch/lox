c6ebc3d73eb6c5fab90e9b6fcd23a82fc5d70e9a
J Palmer <j-mpalmer@live.com>
2023-02-10 19:06:13 -0400

    Secton 4.1.1 Error Handling. Implemented skeleton of an error handling system to be fleshed out later on once the project is more mature

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::args, path::PathBuf, io::{BufRead, Write}};
+use std::{env::args, path::PathBuf, io::{BufRead, Write}, fmt::Display};
 
 
 fn main() {
@@ -9,14 +9,23 @@ fn main() {
         eprintln!("Usage: lox [script]");
         std::process::exit(64);
     } else if args.len() == 2 {
-        run_file(&args[1]).expect("Could not run file");
+        
+        match run_file(&args[1]) {
+            Ok(_) => (),
+            Err(err) => err.error_with_exit() ,
+        };
+        
+        
     } else {
-        run_prompt().expect("Could not run prompt");
+        match run_prompt() {
+            Ok(_) => (),
+            Err(err) => err.error_with_exit(),
+        };
     }
 
 }
 
-fn run_prompt() -> Result<(), std::io::Error> {
+fn run_prompt() -> Result<(), LoxError> {
     
     show_prompt()?;
     
@@ -26,7 +35,10 @@ fn run_prompt() -> Result<(), std::io::Error> {
             if line.is_empty() {
                 break;
             }
-            run(&line)?;
+            match run(&line) {
+                Ok(_) => (),
+                Err(err) => err.report(),
+            };
             show_prompt()?;
             
         } else {
@@ -36,25 +48,33 @@ fn run_prompt() -> Result<(), std::io::Error> {
     
     Ok(())
 }
-fn show_prompt() -> Result<(), std::io::Error> {
+fn show_prompt() -> Result<(), LoxError> {
 
     print!("> ");
-    std::io::stdout().flush()?;
-    Ok(())
+    match std::io::stdout().flush() {
+        Ok(_) => Ok(()),
+        Err(_) => Err(LoxError::new(0, "Could not display prompt")),
+    }
 }
 
-fn run_file(path: impl Into<PathBuf>) -> Result<(), std::io::Error> {
-    let buf = std::fs::read_to_string(path.into())?;
-    run(&buf)?;
-    Ok(())
+fn run_file(path: impl Into<PathBuf>) -> Result<(), LoxError> {
+    
+    if let Ok(buf) = std::fs::read_to_string(path.into()) {
+        match run(&buf) {
+            Ok(_) => Ok(()),
+            Err(err) => Ok(err.error_with_exit()),
+        }
+    }   else {
+        Err(LoxError::new(0, "Could not read file"))
+    }
     
 }
 
 
-fn run(source: &str) -> Result<(), std::io::Error> {
+fn run(source: &str) -> Result<(), LoxError> {
     
     let scanner = Scanner::new(source);
-    let tokens = scanner.scan_tokens();
+    let tokens = scanner.scan_tokens()?;
     
 
     for token in tokens {
@@ -82,15 +102,51 @@ impl Scanner {
 }
 
 impl Scanner {
-    pub fn scan_tokens(&self) -> Vec<Token> {
-        self.source.split_whitespace().map(|s| Token::new(s)).collect()
+    pub fn scan_tokens(&self) -> Result<Vec<Token>, LoxError> {
+        Ok(self.source.split_whitespace().map(|s| Token::new(s)).collect())
     }
 }
 
 impl Token {
     pub fn new(tok: impl Into<String>) -> Self {
-        Token {
+        Self {
             tok: tok.into(),
         }
+    }
+}
+
+
+struct LoxError {
+    line: usize,
+    msg: String, 
+}
+
+impl LoxError {
+
+    pub fn new(line: usize, msg: impl Into<String>) -> Self {
+
+        Self {
+            line,
+            msg: msg.into(),
+        }
+
+    }
+
+    pub fn report(&self) -> () {
+
+        println!("{}", self);
+    }
+
+    pub fn error_with_exit(&self) -> () {
+        self.report();
+        std::process::exit(65);
+    }
+
+
+}
+
+impl Display for LoxError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[Line {}] Error: {}", self.line, self.msg)
     }
 }
